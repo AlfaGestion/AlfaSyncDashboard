84e6ae987c45bad4469708a1d76f0e2d2a13ac72
Alberto Antunez <albertoantunez@hotmail.com>
2026-06-12 16:33:48 -0300

    vs

--- AlfaSyncDashboard/obj/Debug/net8.0-windows/AlfaSyncDashboard.AssemblyInfo.cs
+++ AlfaSyncDashboard/obj/Debug/net8.0-windows/AlfaSyncDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlfaSyncDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e44d28bce3217e32129a8d957f674a39850e902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21beab6f04ddf72a9bc85b929a5a633f5c805e6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlfaSyncDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlfaSyncDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
